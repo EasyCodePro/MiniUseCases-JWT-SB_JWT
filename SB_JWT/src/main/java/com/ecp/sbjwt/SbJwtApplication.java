5fa701fddffcf773e08f9f7cc3025cecb37324b4
prosenjit005 <115132481+EasyCodePro@users.noreply.github.com>
2022-10-07 17:14:35 +0530

    initial commit

---
+++ SB_JWT/src/main/java/com/ecp/sbjwt/SbJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ecp.sbjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJwtApplication.class, args);
+	}
+
+}
